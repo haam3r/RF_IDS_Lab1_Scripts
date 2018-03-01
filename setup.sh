d85a1b8b9a999b51e665fa09702412e07f40fad0
haam3r <andres@cert.ee>
2018-03-01 16:08:41 +0000

    add logging

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-
+echo "Starting LAB services" >> /var/log/syslog
 cp /root/labs/RF_IDS_Lab1_Scripts/systemd/*.service /usr/local/lib/systemd/system/
 systemctl daemon-reload
 cd /root/labs/RF_IDS_Lab1_Scripts/systemd
