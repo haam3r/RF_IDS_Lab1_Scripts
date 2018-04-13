5528c98d3e56cac90d707c3c97721ac2bc30b6c9
haam3r <andres@cert.ee>
2018-04-13 12:54:28 +0000

    update step-5v02 check

--- scripts/step-5v02.py
+++ scripts/step-5v02.py
@@ -16,18 +16,32 @@ def main():
     Check if drop rule works 
     '''
 
-    logging.debug('Starting check for step-3zc0')
     vta_step = 'step-5v02'
     host = 'ids'
     with open("/root/running/red_ips.txt") as f:
         data = f.readlines()
     src = data[-1].rstrip()
 
+    logging.debug('Starting check for {step}'.format(step=vta_step))
+
     response = os.system("ping -c 1 -I " + src + " www")
-
+    nc = os.system("nc -z www 80")
     logging.debug('Response code was {}'.format(response))
+    logging.debug('NC code was {}'.format(nc))
 
-    if response != 0:
+    cmd = 'grep -qP "Drop.+{src}" /var/log/suricata/fast.log; echo $?'.format(src=src)
+    ssh = subprocess.Popen(["ssh", "-o StrictHostKeyChecking=no", host, cmd],
+                           shell=False,
+                           stdout=subprocess.PIPE,
+                           stderr=subprocess.PIPE)
+    try:
+        result = ssh.stdout.readlines()[0].split(" ")
+        logging.debug(result)
+    except IndexError:
+        logging.warning('Suricata not installed on host {host}'.format(host=host))
+        sys.exit(1)
+
+    if response != 0 and result[0].rstrip() == '0' and nc == 0:
         logging.debug("Marking check for {step} as successful".format(step=vta_step))
         command = r"python3 /root/labs/ci-modular-target-checks/objectiveschecks.py -d {step} -y"\
                     .format(step=vta_step)
