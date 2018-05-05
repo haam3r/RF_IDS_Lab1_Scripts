c63d4fc41201af31ec36590541fb58ebf3716a27
haam3r <andres@cert.ee>
2018-05-05 15:41:22 +0000

    move check stuff to py 3 func

---
+++ scripts/objectiveschecks.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+
+__author__ = 'Margus Ernits, Erki Naumanis'
+
+import configparser
+import requests
+import sys
+import json
+import argparse
+from requests.packages.urllib3.util.retry import Retry
+from requests.adapters import HTTPAdapter
+import urllib3
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(levelname)s %(message)s',
+                    filename='/root/running/checks.log', filemode='a')
+
+def check(step, objective):
+    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+    config_filename = '/root/running/lab.ini'
+    print('Reading configuration from:', config_filename)
+    config = configparser.ConfigParser()
+    config.read(config_filename)
+
+    try:
+        ta_key = config.get('LAB', 'ta_key')
+        virtualta_hostname = config.get('LAB', 'virtualta_hostname')
+        lab_id = config.get('LAB', 'lab_id')
+        uid = config.get('LAB', 'uid')
+
+    except Exception:
+        print(" Exception no Ini file or LAB section inside ini file or wrong key values")
+        sys.exit(1)
+
+    # example
+    # curl -H "Content-Type: application/json" -X PUT http://localhost:3013/api/v2/labuser_any/
+    # -d '{"api_key":"9bbbd70dd3a0ffd35f6ee2809bfaee09", "labID":"kDMvZ2DoykiKdJavW", "userID":"MhG4PxWRsJiKNxajx",
+    # "oname":"asd", "score":100, "inc":true}'
+
+    payload = {
+        "api_key": ta_key,
+        "labID": lab_id,
+        "user": uid,
+        "oname": step,
+        "done": objective
+        }
+
+    # Retry mechanism from
+    #  https://www.peterbe.com/plog/best-practice-with-retries-with-requests
+    #  https://stackoverflow.com/questions/15431044/can-i-set-max-retries-for-requests-request
+
+    s = requests.Session()
+    retries = Retry(total=5,
+                    backoff_factor=0.1,
+                    status_forcelist=[500, 502, 503, 504])
+
+    s.mount('http://', HTTPAdapter(max_retries=retries))
+    s.mount('https://', HTTPAdapter(max_retries=retries))
+
+    # Send Obj data to VTA
+    #logging.debug(payload)
+    r = s.put(virtualta_hostname + '/api/v2/labuser_any', json=payload, verify=False)
+    if r.status_code == requests.codes['ok']:
+        return True
+    else:
+
--- scripts/step-rx10.py
+++ scripts/step-rx10.py
@@ -5,6 +5,7 @@ import sys
 import subprocess
 import logging
 import shlex
+from objectiveschecks import check
 
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s %(levelname)s %(message)s',
@@ -24,7 +25,8 @@ def main():
                            stdout=subprocess.PIPE,
                            stderr=subprocess.PIPE)
     try:
-        result = ssh.stdout.readlines()[0].split(" ")
+        result = ssh.stdout.readlines()[0].decode('UTF-8').split(" ")
+        print(result)
     except IndexError:
         logging.warning('Suricata not installed on host {host}'.format(host=host))
         sys.exit(1)
@@ -33,19 +35,20 @@ def main():
 
     if result[0] == 'suricata' and result[1].startswith('4.'):
         logging.info('Correct version of Suricata installed on {host}'.format(host=host))
-        command = r"python3 /root/labs/ci-modular-target-checks/objectiveschecks.py -d {step} -y"\
-                  .format(step=vta_step)
-        p = subprocess.Popen(shlex.split(command),
-                         stdout=subprocess.PIPE,
-                         stderr=subprocess.PIPE,
-                         shell=False)
-        p.communicate()
+        #command = r"python3 /root/labs/ci-modular-target-checks/objectiveschecks.py -d {step} -y"\
+        #          .format(step=vta_step)
+        #p = subprocess.Popen(shlex.split(command),
+        #                 stdout=subprocess.PIPE,
+        #                 stderr=subprocess.PIPE,
+        #                 shell=False)
+        #p.communicate()
 
-        if p.returncode != 0:
+        p = check(vta_step, True)
+        if p == False:
             logging.error('Setting objective step-rx10 completed has failed')
             sys.exit(1)
         else:
-            logging.info('Successfully set step-rx10 as completed: {}'.format(p.returncode))
+            logging.info('Successfully set {step} as completed'.format(step=vta_step))
     else:
         logging.info('Check failed for unknown reasons')
         sys.exit(1)
